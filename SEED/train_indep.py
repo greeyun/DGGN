4849d7a2843f6322d7b92a12fc7aba1f6de91c31
greeyun <68091618+greeyun@users.noreply.github.com>
2023-02-02 20:23:19 +0800

    python3

--- SEED/train_indep.py
+++ SEED/train_indep.py
@@ -1,11 +1,10 @@
 import torch
-import SEED_config as config
 import numpy as np
 from torch import nn
 from torch.utils.data.dataloader import DataLoader
-from SEED_model import Generator, Discriminator, Classfication
-from SEED_dataloader import Mydataset, get_train_test_sub
-from SEED_utils import EarlyStopping_gan, plot_confusion_matrix, schedule
+from model import Generator, Discriminator, Classfication
+from dataloader import Mydataset, get_train_test_sub
+from utils import EarlyStopping_gan, plot_confusion_matrix, schedule
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 
